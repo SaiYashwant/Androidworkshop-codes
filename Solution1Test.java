bca14b26d944aca8462704a085316f2a31a3a7df
SaiYashwant <saiyashwantchavan@gmail.com>
2016-09-19 11:50:39 +0530

    Assignment 1, Day 2

---
+++ Solution1Test.java
@@ -0,0 +1,17 @@
+class Solution1
+{
+public boolean sameLast(int a,int b)
+{
+return(a%10==b%10);
+}
+}
+public class Solution1Test
+{
+public static void main(String args[])
+{
+Solution1 ob = new Solution1();
+System.out.println(ob.sameLast(25,35));
+System.out.println(ob.sameLast(21,39));
+System.out.println(ob.sameLast(77,7));    
+}
+}
